93e1614ffb3e0df80b92bd0163675485f0f94b6c
Yoel Marti <yoel120491@gmail.com>
2021-10-22 01:13:26 +0200

    Iteration#3

---
+++ iteracion-3.js
@@ -0,0 +1,35 @@
+// Iteración#3
+
+// Ejercicio 1.1
+
+let multiplicacion = 10*5;
+
+console.log(multiplicacion);
+
+// Ejercicio 1.2
+
+let division = 10/2;
+
+console.log(division);
+
+// Ejercicio 1.3
+
+let restoDivision = 15%9;
+
+console.log(restoDivision);
+
+// Ejercicio 1.4
+
+let y = 10;
+let z = 5;
+
+let x = y + z;
+
+// Ejercicio 1.5
+
+let y = 10;
+let z = 5;
+
+let x = y * z;
+
+console.log(x);
